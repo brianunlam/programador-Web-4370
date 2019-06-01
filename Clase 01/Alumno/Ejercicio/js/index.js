847b742bf9200213072b3ae245fccb809e598936
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-06-01 13:46:58 -0300

    1er subida del 1er ejercicio

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,24 @@
+// Parte 1º
+alert('todo esta re piola?')
+var nombre = prompt('ingrese nombre')
+console.log('hola bienvenido: '+ nombre)
+
+// Parte 2º
+var pi = 3.14
+var numeroASumar = prompt('ingrese un Numero')
+numeroASumar = parseFloat(numeroASumar)
+console.log(pi + numeroASumar)
+
+// Parte 3º
+var pi = 3.14
+var numeroARestar = prompt('ingrese un Numero')
+numeroARestar = parseFloat(numeroARestar)
+console.log(pi - numeroARestar)
+
+// Parte 4º
+var pi = 3.14
+var numeroASumar = prompt('ingrese un Numero')
+numeroASumar = parseFloat(numeroASumar)
+var numeroASumar = prompt('ingrese otro Numero')
+numeroASumar = parseFloat(numeroASumar)
+console.log(pi + numeroASumar)
